b6c5f09588259221a99f0398873cee0fdf50e8f6
Ildar Gilyazev <i.gilyazev@yandex.ru>
2023-09-20 17:44:24 +0000

    add grafana deb package

--- all_deb/iptables.sh
+++ all_deb/iptables.sh
@@ -3,8 +3,8 @@ iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -s 127.0.0.53 -j ACCEPT
 iptables -A INPUT -d 127.0.0.53 -j ACCEPT
 iptables -A INPUT -p udp --dport 53 -j ACCEPT
-iptables -A INPUT -s 10.128.0.0/24 -j ACCEPT
-iptables -A INPUT -d 10.128.0.0/24 -j ACCEPT
+iptables -A INPUT -s 10.0.0.0/8 -j ACCEPT
+iptables -A INPUT -d 10.0.0.0/8 -j ACCEPT
 iptables -A INPUT -p tcp --dport 80 -s 10.0.0.0/8 -j ACCEPT
 iptables -A INPUT -j DROP
 
